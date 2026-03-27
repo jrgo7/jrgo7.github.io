c572a7e2ecc7d8ae4fbaeccc3155ec11a1590258
Justin Rainier Go <jrgo.dev@gmail.com>
2026-03-28 01:55:44 +0800

    Update resume and misc details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 async function switchLayout(layoutName) {
-    layoutHtml = await (await fetch(`layouts/${layoutName}.html`)).text();
-    document.querySelector('main').innerHTML = layoutHtml;
+  layoutHtml = await (await fetch(`layouts/${layoutName}.html`)).text();
+  document.querySelector("main").innerHTML = layoutHtml;
 
-    document.querySelectorAll("a.switch-layout").forEach(a => {
-        a.onclick = async () => switchLayout(a.dataset.layout)
-    })
-
+  allLinks = document.querySelectorAll("a.switch-layout");
+  allLinks.forEach((a) => {
+    a.onclick = async () => {
+      switchLayout(a.dataset.layout);
+      allLinks.forEach((a) => (a.style.fontWeight = "normal"));
+      a.style.fontWeight = "bold";
+    };
+  });
 }
 
-
-
-switchLayout('home')
+switchLayout("home");
